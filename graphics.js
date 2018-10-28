dd4564744aaa0a22a0fc9765aa0ac10df7afd3fc
Isidoro Ghezzi <isidoro.ghezzi@icloud.com>
2018-10-28 16:55:24 +0100

    in DEBUG show the mass

--- graphics.js
+++ graphics.js
@@ -111,6 +111,7 @@ function updateData() {
 		for(i=0;i<bods.N;i++){
 			data.innerHTML += "<li> B"+i+" : Pos "+
 				bods.pos.x[i].toFixed(2)+", "+bods.pos.y[i].toFixed(2)+
+				"; M: " + bods.mass[i] +
 				" </li>";
 		}
 		data.innerHTML += "</ul>";
